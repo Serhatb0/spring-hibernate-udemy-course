cc36c26273c207ea98503bfe0dcad48e45c31243
Serhat Biricik <biricikserhat47@gmail.com>
2022-06-17 19:28:43 +0300

    Injecting Literal Values

--- spring-demo-one/src/com/biricik/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/biricik/springdemo/CricketCoach.java
@@ -4,6 +4,11 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// add new fields for emailAddress and team
+	private String emailAddress;
+	private String team;
+	
+	
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
@@ -25,5 +30,24 @@ public class CricketCoach implements Coach {
 	public String getDailyFortune() {
 		return this.fortuneService.getFortune();
 	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
+	}
+
 
 }
