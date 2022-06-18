5a27ad1bf43e1e9b536d390fa4e6148cb40f2865
Serhat Biricik <biricikserhat47@gmail.com>
2022-06-18 21:42:36 +0300

    spring-demo-annotations

---
+++ spring-demo.annotations/src/com/biricik/springdemo/TennisCoach.java
@@ -0,0 +1,13 @@
+	package com.biricik.springdemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
