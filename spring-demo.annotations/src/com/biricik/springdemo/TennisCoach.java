842bde730a364e2c9fb7236bf43fb3f85c33a216
Serhat Biricik <biricikserhat47@gmail.com>
2022-06-19 21:05:24 +0300

    Annotation Bean Scope

--- spring-demo.annotations/src/com/biricik/springdemo/TennisCoach.java
+++ spring-demo.annotations/src/com/biricik/springdemo/TennisCoach.java
@@ -3,9 +3,11 @@ package com.biricik.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class TennisCoach implements Coach {
 
 	// define a email and a team
