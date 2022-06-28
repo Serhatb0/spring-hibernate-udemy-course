4576c2436cff75958cda0dc530e38675627bea00
Serhat Biricik <biricikserhat47@gmail.com>
2022-06-28 11:59:53 +0300

    Hibernate OneToOne

---
+++ hb-02-one-to-one-bi/src/com/biricik/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package com.biricik.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+	
+	public static void main(String[] args) {
+		
+		String jdbcUrl = "jdbc:postgresql://localhost:5432/spring-hibernate";
+		String user = "postgres";
+		String password = "123456";
+		
+		
+		try {
+			System.out.println("Connecting to database: " + jdbcUrl);
+			
+			Connection myConn = DriverManager.getConnection(jdbcUrl,user,password);
+			
+			System.out.println("Connection Successful");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
