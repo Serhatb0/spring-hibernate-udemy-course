b107afa69006cc85113354f828f40bf683d74dc0
Serhat Biricik <biricikserhat47@gmail.com>
2022-07-02 01:21:35 +0300

    Hibernate many-to-many

---
+++ hb-05-many-to-many/src/com/biricik/hibernate/demo/AddCoursesForSerhatDemo.java
@@ -0,0 +1,62 @@
+package com.biricik.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.biricik.hibernate.demo.entity.Course;
+import com.biricik.hibernate.demo.entity.Instructor;
+import com.biricik.hibernate.demo.entity.InstructorDetail;
+import com.biricik.hibernate.demo.entity.Review;
+import com.biricik.hibernate.demo.entity.Student;
+
+public class AddCoursesForSerhatDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(Review.class).addAnnotatedClass(Student.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			// get the student serhat from database
+			int studentId = 6;
+			Student tempStudent = session.get(Student.class, studentId);
+
+			System.out.println("\nLoaded student: " + tempStudent);
+			System.out.println("Courses: " + tempStudent.getCourses());
+
+			// create more courses
+			Course tempCourse1 = new Course("Rubik's Cube - How to Speed Cube");
+			Course tempCourse2 = new Course("Atari 2600 - Game Development");
+
+			// add student to courses
+			tempCourse1.addStudent(tempStudent);
+			tempCourse2.addStudent(tempStudent);
+
+			// save the courses
+			System.out.println("\nSaving the courses ...");
+
+			session.save(tempCourse1);
+			session.save(tempCourse2);
+
+			// commit transaction
+			session.getTransaction().commit();
+
+			System.out.println("Done!");
+		} finally {
+			// add clean up code
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
