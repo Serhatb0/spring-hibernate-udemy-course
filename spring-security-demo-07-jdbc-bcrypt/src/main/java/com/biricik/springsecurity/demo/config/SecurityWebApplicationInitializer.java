471702c6813654fd7a75cccfb4b995537d71b6f0
Serhat Biricik <biricikserhat47@gmail.com>
2022-08-01 13:13:12 +0300

    spring-security-demo-jdbc-bcrypt

---
+++ spring-security-demo-07-jdbc-bcrypt/src/main/java/com/biricik/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.biricik.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ spring-security-demo-07-jdbc-bcrypt/src/main/java/com/biricik/springsecurity/demo/controller/LoginController.java
@@ -0,0 +1,21 @@
+package com.biricik.springsecurity.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/showMyLoginPage")
+	public String showMyLoginPage() {
+		
+//		return "plain-login";
+		return "fancy-login";
+	}
+	
+	@GetMapping("/access-denied")
+	public String showAccessDenied() {
+		return "access-denied";
+	}
+		
+}
