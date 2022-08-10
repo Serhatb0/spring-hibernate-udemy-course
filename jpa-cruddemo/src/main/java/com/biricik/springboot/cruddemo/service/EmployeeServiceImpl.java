218a77057939dac0ca7f541f585c1d85b3b64999
Serhat Biricik <biricikserhat47@gmail.com>
2022-08-10 13:18:26 +0300

    spring-boot-crud-api-with-jpa

---
+++ jpa-cruddemo/src/main/java/com/biricik/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,47 @@
+package com.biricik.springboot.cruddemo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.biricik.springboot.cruddemo.dao.EmployeeDAO;
+import com.biricik.springboot.cruddemo.entity.Employee;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+	
+	private EmployeeDAO employeeDAO;
+	
+	@Autowired
+	public EmployeeServiceImpl(@Qualifier("employeeDAOJpaImpl") EmployeeDAO employeeDAO) {
+		this.employeeDAO = employeeDAO;
+	}
+
+	@Override
+	@Transactional
+	public List<Employee> findAll() {
+		return employeeDAO.findAll();
+	}
+
+	@Override
+	@Transactional
+	public Employee findById(int id) {
+		return employeeDAO.findById(id);
+	}
+
+	@Override
+	@Transactional
+	public void save(Employee employee) {
+		employeeDAO.save(employee);
+	}
+
+	@Override
+	@Transactional
+	public void deleteById(int id) {
+		employeeDAO.deleteById(id);
+	}
+
+}
